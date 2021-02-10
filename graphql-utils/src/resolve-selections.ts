021f781eb956496268ffa5fe350bab4289faee79
Dan6erbond <moravrav@gmail.com>
2021-02-10 13:10:57 +0100

    Create resolve-selections.ts

---
+++ graphql-utils/src/resolve-selections.ts
@@ -0,0 +1,53 @@
+import { GraphQLResolveInfo } from "graphql";
+import { hasFields } from "./has-fields";
+
+export interface FieldSelections {
+  field: string;
+  selector?: string;
+  selections?: (string | FieldSelections)[];
+}
+
+export const resolveSelections = (
+  fields: (string | FieldSelections)[],
+  info: GraphQLResolveInfo,
+  selections: string[] = [],
+  parent?: string,
+) => {
+  let resolvedSelections = [...selections];
+
+  for (const fieldSelection of fields) {
+    let field: string;
+    let selector: string | undefined;
+
+    if (typeof fieldSelection === "string") {
+      field = fieldSelection;
+      selector = field;
+    } else {
+      field = fieldSelection.field;
+      if (fieldSelection.selector) {
+        selector = fieldSelection.selector;
+      }
+    }
+
+    if (parent) {
+      field = [...parent.split("."), ...field.split(".")].join(".");
+    }
+
+    if (hasFields(field, info)) {
+      if (selector) {
+        resolvedSelections = [...resolvedSelections, selector];
+      }
+
+      if (typeof fieldSelection !== "string" && fieldSelection.selections) {
+        resolvedSelections = resolveSelections(
+          fieldSelection.selections,
+          info,
+          resolvedSelections,
+          field,
+        );
+      }
+    }
+  }
+
+  return resolvedSelections;
+};
