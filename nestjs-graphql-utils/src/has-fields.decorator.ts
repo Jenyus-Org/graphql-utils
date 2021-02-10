3c4d5d3768315f55053496e8c88d450bf5c9d5eb
Dan6erbond <moravrav@gmail.com>
2021-02-10 13:17:04 +0100

    Create has-fields.decorator.ts

---
+++ nestjs-graphql-utils/src/has-fields.decorator.ts
@@ -0,0 +1,18 @@
+import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+import { GqlExecutionContext } from "@nestjs/graphql";
+import { hasFields } from "@jenyus/graphql-utils";
+
+export const HasFields = (...fields: string[]) => {
+  return createParamDecorator<string[], ExecutionContext, boolean>(
+    (search, context) => {
+      const ctx = GqlExecutionContext.create(context);
+      const info = ctx.getInfo();
+      for (const field of search) {
+        if (!hasFields(field, info)) {
+          return false;
+        }
+      }
+      return true;
+    }
+  )(fields);
+};
