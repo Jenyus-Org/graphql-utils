16480bede3985b73243215340c2d04b8580335f3
Dan6erbond <moravrav@gmail.com>
2021-02-11 11:19:09 +0100

    Write test for `resolveSelections`

--- graphql-utils/src/resolve-selections.test.ts
+++ graphql-utils/src/resolve-selections.test.ts
@@ -65,4 +65,60 @@ describe("Resolving relationships from GraphQL query fields.", () => {
     expect(relations).to.have.length(expectedRelations.length);
     expect(relations).to.have.members(expectedRelations);
   });
+
+  it("Must work for deeply nested selectors.", () => {
+    const fields: FieldSelections[] = [
+      {
+        field: "user",
+        selections: [
+          {
+            field: "otherField",
+            selections: ["user.username"],
+          },
+        ],
+      },
+    ];
+
+    const { definitions } = parse(`{
+      user {
+        otherField {
+          moreUnrelatedFields
+          user {
+            username
+          }
+        }
+      }
+    }`);
+    const operation = definitions.find(
+      ({ kind }) => kind === "OperationDefinition"
+    );
+
+    const {
+      selectionSet: { selections },
+    } = operation as OperationDefinitionNode;
+
+    const fragments = definitions
+      .filter(({ kind }) => kind === "FragmentDefinition")
+      .reduce(
+        (result, current) => ({
+          ...result,
+          [(current as FragmentDefinitionNode).name.value]: current,
+        }),
+        {}
+      );
+
+    const info = {
+      fragments,
+      fieldNodes: selections as FieldNode[],
+    };
+
+    const relations = resolveSelections(
+      fields,
+      (info as unknown) as GraphQLResolveInfo
+    );
+    const expectedRelations = ["user.username"];
+
+    expect(relations).to.have.length(expectedRelations.length);
+    expect(relations).to.have.members(expectedRelations);
+  });
 });
