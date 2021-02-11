731f83cedbd29a11ef12e4b1e14c643d56a6eaef
Dan6erbond <moravrav@gmail.com>
2021-02-11 11:19:01 +0100

    Write test for `resolveSelections`

---
+++ graphql-utils/src/resolve-selections.test.ts
@@ -0,0 +1,68 @@
+import { expect } from "chai";
+import {
+  FieldNode,
+  FragmentDefinitionNode,
+  GraphQLResolveInfo,
+  OperationDefinitionNode,
+  parse,
+} from "graphql";
+import { describe } from "mocha";
+import { FieldSelections, resolveSelections } from "./resolve-selections";
+
+describe("Resolving relationships from GraphQL query fields.", () => {
+  it("Should resolve given relations for entered fields.", () => {
+    const fields: FieldSelections[] = [
+      {
+        field: "items",
+        selections: ["tasks", "tasks.activities", "tasks.user"],
+      },
+    ];
+
+    const { definitions } = parse(`{
+      projects(search: "Test") {
+        id
+        items {
+          tasks {
+            activities {
+              id
+            }
+            user {
+              id
+            }
+          }
+        }
+      }
+    }`);
+    const operation = definitions.find(
+      ({ kind }) => kind === "OperationDefinition"
+    );
+
+    const {
+      selectionSet: { selections },
+    } = operation as OperationDefinitionNode;
+
+    const fragments = definitions
+      .filter(({ kind }) => kind === "FragmentDefinition")
+      .reduce(
+        (result, current) => ({
+          ...result,
+          [(current as FragmentDefinitionNode).name.value]: current,
+        }),
+        {}
+      );
+
+    const info = {
+      fragments,
+      fieldNodes: selections as FieldNode[],
+    };
+
+    const relations = resolveSelections(
+      fields,
+      (info as unknown) as GraphQLResolveInfo
+    );
+    const expectedRelations = ["tasks", "tasks.activities", "tasks.user"];
+
+    expect(relations).to.have.length(expectedRelations.length);
+    expect(relations).to.have.members(expectedRelations);
+  });
+});
