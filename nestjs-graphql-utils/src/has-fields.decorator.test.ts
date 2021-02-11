e57e54ee2d99b59974e9ded45c63c7eecb49144e
Dan6erbond <moravrav@gmail.com>
2021-02-11 12:03:30 +0100

    Create `getGqlExecutionContext` helper

--- nestjs-graphql-utils/src/has-fields.decorator.test.ts
+++ nestjs-graphql-utils/src/has-fields.decorator.test.ts
@@ -1,58 +1,21 @@
 import { expect } from "chai";
-import {
-  FieldNode,
-  FragmentDefinitionNode,
-  GraphQLResolveInfo,
-  OperationDefinitionNode,
-  parse,
-} from "graphql";
 import { describe } from "mocha";
 import { HasFields } from "./has-fields.decorator";
-import { getParamDecoratorFactory } from "./helpers";
-import * as httpMock from "node-mocks-http";
-import { ExecutionContextHost } from "@nestjs/core/helpers/execution-context-host";
+import { getGqlExecutionContext, getParamDecoratorFactory } from "./helpers";
 
 describe("Resolving selectors from GraphQL query fields.", () => {
   it("Must work for deeply nested selectors.", () => {
-    const query = `{
+    const ctx = getGqlExecutionContext(`{
       user {
         username
         firstName
         lastName
       }
-    }`;
-    const { definitions } = parse(query);
-    const operation = definitions.find(
-      ({ kind }) => kind === "OperationDefinition"
-    );
+    }`);
 
-    const {
-      selectionSet: { selections },
-    } = operation as OperationDefinitionNode;
+    const hasFields = getParamDecoratorFactory(HasFields);
 
-    const fragments = definitions
-      .filter(({ kind }) => kind === "FragmentDefinition")
-      .reduce(
-        (result, current) => ({
-          ...result,
-          [(current as FragmentDefinitionNode).name.value]: current,
-        }),
-        {}
-      );
-
-    const info = ({
-      fragments,
-      fieldNodes: selections as FieldNode[],
-    } as unknown) as GraphQLResolveInfo;
-
-    const factory = getParamDecoratorFactory(HasFields);
-
-    const req = httpMock.createRequest();
-
-    const res = httpMock.createResponse();
-    const ctx = new ExecutionContextHost([req, res, null, info]);
-
-    const fieldsFound = factory(
+    const fieldsFound = hasFields(
       ["user.username", "user.firstName", "user.lastName"],
       ctx
     );
--- nestjs-graphql-utils/src/helpers.ts
+++ nestjs-graphql-utils/src/helpers.ts
@@ -1,5 +1,15 @@
-import "reflect-metadata";
 import { ROUTE_ARGS_METADATA } from "@nestjs/common/constants";
+import { ExecutionContextHost } from "@nestjs/core/helpers/execution-context-host";
+import {
+  FieldNode,
+  FragmentDefinitionNode,
+  GraphQLResolveInfo,
+  OperationDefinitionNode,
+  parse,
+} from "graphql";
+import * as httpMock from "node-mocks-http";
+import "reflect-metadata";
+import { HasFields } from "./has-fields.decorator";
 
 export function getParamDecoratorFactory(decorator: Function) {
   class Test {
@@ -8,4 +18,41 @@ export function getParamDecoratorFactory(decorator: Function) {
 
   const args = Reflect.getMetadata(ROUTE_ARGS_METADATA, Test, "test");
   return args[Object.keys(args)[0]].factory;
+}
+
+export function getGraphQLResolveInfo(query: string) {
+  const { definitions } = parse(query);
+  const operation = definitions.find(
+    ({ kind }) => kind === "OperationDefinition"
+  );
+
+  const {
+    selectionSet: { selections },
+  } = operation as OperationDefinitionNode;
+
+  const fragments = definitions
+    .filter(({ kind }) => kind === "FragmentDefinition")
+    .reduce(
+      (result, current) => ({
+        ...result,
+        [(current as FragmentDefinitionNode).name.value]: current,
+      }),
+      {}
+    );
+
+  const info = {
+    fragments,
+    fieldNodes: selections as FieldNode[],
+  };
+
+  return (info as unknown) as GraphQLResolveInfo;
+}
+
+export function getGqlExecutionContext(query: string) {
+  const req = httpMock.createRequest();
+  const res = httpMock.createResponse();
+
+  const info = getGraphQLResolveInfo(query);
+
+  return new ExecutionContextHost([req, res, null, info]);
 }
