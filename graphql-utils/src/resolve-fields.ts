29b377da4e8d5a66f2ba30a673b26e09637cb27e
Dan6erbond <moravrav@gmail.com>
2021-02-13 16:08:48 +0100

    Use `resolveFieldMap` in `resolveFields`

--- graphql-utils/src/resolve-fields.ts
+++ graphql-utils/src/resolve-fields.ts
@@ -1,103 +1,12 @@
-import { FieldNode, FragmentDefinitionNode, GraphQLResolveInfo } from "graphql";
-import { FragmentDict } from "./helpers";
+import { GraphQLResolveInfo } from "graphql";
+import { fieldMapToDot } from "./helpers";
+import { resolveFieldMap } from "./resolve-field-map";
 
 export const resolveFields = (
   info: Pick<GraphQLResolveInfo, "fieldNodes" | "fragments">,
   deep: boolean = true,
-  parent: string | string[] = ""
+  parent: string | string[] = []
 ) => {
-  const { fieldNodes, fragments } = info;
-  let fields: string[] = [];
-
-  if (parent) {
-    const parents = Array.isArray(parent) ? parent : parent.split(".");
-    for (const fieldNode of fieldNodes) {
-      if (fieldNode.name.value === parent) {
-        parents.shift();
-        const resolvedFields = resolveFieldsFromSelectionSet(
-          fieldNode,
-          deep,
-          parents.join("."),
-          fragments
-        );
-        fields = [...fields, ...resolvedFields];
-      }
-    }
-    return fields;
-  }
-
-  for (const fieldNode of fieldNodes) {
-    if (!parent) {
-      fields = [...fields, fieldNode.name.value];
-    }
-    const resolvedFields = resolveFieldsFromSelectionSet(
-      fieldNode,
-      deep,
-      parent,
-      fragments,
-      [fieldNode.name.value]
-    );
-    fields = [...fields, ...resolvedFields];
-  }
-
-  return fields;
-};
-
-const resolveFieldsFromSelectionSet = (
-  selectionNode: FieldNode | FragmentDefinitionNode,
-  deep: boolean = true,
-  parent: string | string[] = "",
-  fragments: FragmentDict,
-  fieldParent: string[] = [],
-  fields: string[] = []
-) => {
-  if (parent) {
-    const parents = Array.isArray(parent) ? parent : parent.split(".");
-    for (const fieldNode of selectionNode.selectionSet.selections) {
-      if (fieldNode.kind === "Field") {
-        if (fieldNode.name.value === parents[0]) {
-          parents.shift();
-          const resolvedFields = resolveFieldsFromSelectionSet(
-            fieldNode,
-            deep,
-            parents.join("."),
-            fragments
-          );
-          fields = [...fields, ...resolvedFields];
-        }
-      }
-    }
-    return fields;
-  }
-
-  if (selectionNode.selectionSet) {
-    for (const selection of selectionNode.selectionSet.selections) {
-      if (selection.kind === "Field") {
-        const field = [...fieldParent, selection.name.value].join(".");
-        fields = [...fields, field];
-        if (deep) {
-          const resolvedFields = resolveFieldsFromSelectionSet(
-            selection,
-            deep,
-            parent,
-            fragments,
-            [...fieldParent, selection.name.value]
-          );
-          fields = [...fields, ...resolvedFields];
-        }
-      } else if (selection.kind === "FragmentSpread") {
-        const fragment = fragments[selection.name.value];
-        const resolvedFields = resolveFieldsFromSelectionSet(
-          fragment,
-          deep,
-          parent,
-          fragments,
-          [...fieldParent, selection.name.value]
-        );
-        fields = [...fields, ...resolvedFields];
-      }
-    }
-  }
-
-  return fields;
+  const fieldMap = resolveFieldMap(info, parent, deep);
+  return fieldMapToDot(fieldMap);
 };
