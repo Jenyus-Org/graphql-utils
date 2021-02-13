485931977ff87c6fb920ae262f8632f5f2e0010f
Dan6erbond <moravrav@gmail.com>
2021-02-13 16:09:22 +0100

    Use `resolveFieldMap` in `resolveFields`

--- graphql-utils/src/has-fields.ts
+++ graphql-utils/src/has-fields.ts
@@ -4,7 +4,8 @@ import {
   GraphQLResolveInfo,
   SelectionSetNode,
 } from "graphql";
-import { FragmentDict } from "./helpers";
+import { fieldMapToDot, FragmentDict } from "./helpers";
+import { resolveFieldMap } from "./resolve-field-map";
 
 /**
  * Searches the GraphQLResolveInfo for selectors that match the search string or array, and returns `true` if found.
@@ -24,89 +25,22 @@ import { FragmentDict } from "./helpers";
  * @returns `true` if the fields were located in the query as directly nested selectors, otherwise `false`.
  */
 export const hasFields = (
+  info: Pick<GraphQLResolveInfo, "fieldNodes" | "fragments">,
   search: string | string[],
-  info: Pick<GraphQLResolveInfo, "fieldNodes" | "fragments">
+  atRoot: boolean = false
 ) => {
-  const { fieldNodes, fragments } = info;
-  const fields = Array.isArray(search) ? search : search.split(".");
+  const field = Array.isArray(search) ? search.join(".") : search;
+  const fieldMap = resolveFieldMap(info);
+  const fields = fieldMapToDot(fieldMap);
 
-  for (const fieldNode of fieldNodes) {
-    if (!fieldNode.selectionSet) {
-      continue;
-    }
-
-    const found = hasFieldName(fieldNode, fields, fragments);
-    if (found) {
-      return true;
-    }
-  }
-
-  return false;
-};
-
-const hasFieldName = (
-  selectionNode: FieldNode | FragmentDefinitionNode,
-  search: string[],
-  fragments: FragmentDict
-) => {
-  if (search.length === 0) {
-    return true;
-  }
-
-  if (selectionNode.name.value === search[0]) {
-    const newSearch = [...search];
-    newSearch.shift();
-    if (selectionNode.selectionSet) {
-      if (hasFieldSet(selectionNode.selectionSet, newSearch, fragments)) {
+  if (atRoot) {
+    return fields.includes(field);
+  } else {
+    for (const fieldDot of fields) {
+      if (fieldDot.indexOf(field) !== -1) {
         return true;
       }
     }
   }
-
-  if (selectionNode.selectionSet) {
-    for (const selection of selectionNode.selectionSet.selections) {
-      if (selection.kind === "Field") {
-        if (hasFieldName(selection, search, fragments)) {
-          return true;
-        }
-      } else if (selection.kind === "FragmentSpread") {
-        const fragment = fragments[selection.name.value];
-        if (hasFieldName(fragment, search, fragments)) {
-          return true;
-        }
-      }
-    }
-  }
-
-  return false;
-};
-
-const hasFieldSet = (
-  selectionSet: SelectionSetNode,
-  search: string[],
-  fragments: FragmentDict
-): boolean => {
-  if (search.length === 0) {
-    return true;
-  }
-
-  for (const selection of selectionSet.selections) {
-    if (selection.kind === "Field" && selection.name.value === search[0]) {
-      search.shift();
-      if (selection.selectionSet) {
-        if (hasFieldSet(selection.selectionSet, search, fragments)) {
-          return true;
-        }
-      } else if (search.length === 0) {
-        return true;
-      }
-    } else if (selection.kind === "FragmentSpread") {
-      const fragment = fragments[selection.name.value];
-      if (hasFieldSet(fragment.selectionSet, search, fragments)) {
-        return true;
-      }
-    }
-  }
-
   return false;
 };
