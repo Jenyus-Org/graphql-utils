313a62b5d542f47be947bf908df19174fb48aa0a
Dan6erbond <moravrav@gmail.com>
2021-02-13 22:42:48 +0100

    Fix exports, version bump to `1.4.1` for `nestjs-graphql-utils`

--- nestjs-graphql-utils/src/index.ts
+++ nestjs-graphql-utils/src/index.ts
@@ -1,2 +1,4 @@
 export { HasFields } from "./has-fields.decorator";
 export { Selections } from "./selections.decorator";
+export { Fields } from "./fields.decorator";
+export { FieldMap } from "./field-map.decorator";
