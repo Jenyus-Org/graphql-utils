8b1d8a7f02918f070b8e0b1e5e6aa1a58a5fc49c
Dan6erbond <moravrav@gmail.com>
2021-03-24 17:01:14 +0100

    Add support for inline fragments with tests

--- graphql-utils/src/resolve-field-map.test.ts
+++ graphql-utils/src/resolve-field-map.test.ts
@@ -68,4 +68,23 @@ describe("Resolving all selected fields in a GraphQL query", () => {
 
     expect(fields).to.deep.equal({ user: {} });
   });
+
+  it("Must work with inline fragments", () => {
+    const info = getGraphQLResolveInfo(`{
+      user {
+        ...on User {
+          id
+          username
+          firstName
+          lastName
+        }
+      }
+    }`);
+
+    const fields = resolveFieldMap(info);
+
+    expect(fields).to.deep.equal({
+      user: { id: {}, username: {}, firstName: {}, lastName: {} },
+    });
+  });
 });
